7abad8e5f8f42b7239492fd996f79191679db996
Sydney Grant <sg3957@nyu.edu>
2018-09-29 17:19:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,13 +10,14 @@ end
 
 
 def long_planeteer_calls(calls)
-  calls.each { |call|
-    if call.size > 4
-      return true
-    else
-      return false
+  calls.any?{|x| x > 4}
+  # calls.each { |call|
+  #   if call.size > 4
+  #     return true
+  #   else
+  #     return false
     
-  end}
+  # end}
 end
 
 # def long_planeteer_calls(calls)
